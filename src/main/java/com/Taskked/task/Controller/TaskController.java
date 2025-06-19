697a83eba15cb8f32b10f4e67ef9d0d88e1aa209
supalvasani <supalvasani@gmail.com>
2025-06-19 16:07:47 +0530

    service in progress

--- src/main/java/com/Taskked/task/Controller/TaskController.java
+++ src/main/java/com/Taskked/task/Controller/TaskController.java
@@ -1,19 +1,55 @@
 package com.Taskked.task.Controller;
 
+import com.Taskked.task.DTO.TaskRequestDTO;
 import com.Taskked.task.DTO.TaskResponseDTO;
+import com.Taskked.task.Model.Tasks;
 import com.Taskked.task.Service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
 public class TaskController {
     @Autowired
     private TaskService taskService;
 
-    @PostMapping
-    public ResponseEntity<?> createTask(@RequestBody TaskResponseDTO taskResponseDTO){
-        return new ResponseEntity<>(taskResponseDTO, HttpStatus.ACCEPTED);
+    @PostMapping("/task")
+    public ResponseEntity<?> createTask(@RequestBody TaskRequestDTO taskResponseDTO){
+        return new ResponseEntity<>(taskService.createTask(taskResponseDTO), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/users/{id}/tasks")
+    public ResponseEntity<?> getAllTask(@PathVariable("id") String userid){
+        List<Tasks> task = taskService.getAllTask(userid);
+        return new ResponseEntity<>(task,HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<?> UpdateTasks(@RequestBody TaskResponseDTO taskResponseDTO){
+        Tasks task = taskService.updateTask(taskResponseDTO);
+        return new ResponseEntity<>(task,HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<?> deleteTask(@RequestBody TaskResponseDTO taskResponseDTO){
+        return new ResponseEntity<>(taskService.deleteTask(taskResponseDTO,HttpStatus.ACCEPTED));
+    }
+
+    @PutMapping("/tasks/{id}/complete")
+    public ResponseEntity<?> completedTask(@RequestBody TaskResponseDTO taskResponseDTO){
+        return new ResponseEntity<>(taskService.completeTask(taskResponseDTO,HttpStatus.ACCEPTED));
+    }
+
+    @GetMapping("/users/{id}/tasks/completed")
+    public ResponseEntity<?> completedTasks(@PathVariable("id") String userid){
+        return new ResponseEntity<>(taskService.completeTask(userid),HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/users/{id}/tasks/incomplete")
+    public ResponseEntity<?> incompleteTasks(@PathVariable("id") String userid){
+        return new ResponseEntity<>(taskService.incompleteTask(userid),HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/Taskked/task/Service/TaskService.java
+++ src/main/java/com/Taskked/task/Service/TaskService.java
@@ -1,7 +1,70 @@
 package com.Taskked.task.Service;
 
+import com.Taskked.task.DTO.TaskRequestDTO;
+import com.Taskked.task.DTO.TaskResponseDTO;
+import com.Taskked.task.Exception.InvalidTaskException;
+import com.Taskked.task.Exception.TaskNotFoundException;
+import com.Taskked.task.Model.Tasks;
+import com.Taskked.task.Repo.TaskRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepo taskRepo;
+    public List<Tasks> getAllTask(String userid) {
+        List<Tasks> taskList = taskRepo.alltaskd(userid);
+        if (taskList.isEmpty()) {
+            throw new TaskNotFoundException("No tasks found for user: " + userid);
+        }
+        return taskList;
+    }
+
+    public Object createTask(TaskRequestDTO taskRequestDTO) {
+        Tasks task = new Tasks();
+        task.setTitle(taskRequestDTO.getTitle());
+        task.setCompleted(taskRequestDTO.isCompleted());
+        task.setUserId(taskRequestDTO.getUserId());
+        int saved = taskRepo.addTasks(task);
+        if (saved <= 0) {
+            throw new InvalidTaskException("task not created");
+        }
+        return task;
+    }
+
+    public Tasks updateTask(TaskResponseDTO taskResponseDTO) {
+        Tasks task = new Tasks();
+        if (taskResponseDTO.getTitle() == null || taskResponseDTO.getTitle().trim().isEmpty()) {
+            throw new InvalidTaskException("Title can't be empty");
+        }
+        if (taskResponseDTO.getTaskid() == 0) {
+            throw new InvalidTaskException("Task ID is missing");
+        }
+        task.setTitle(taskResponseDTO.getTitle());
+        task.setCompleted(taskResponseDTO.isCompleted());
+        int updated = taskRepo.updateTask(task);
+        if (updated <= 0) {
+            throw new InvalidTaskException("Task update failed");
+        }
+        return task;
+    }
+
+    public HttpStatusCode deleteTask(TaskResponseDTO taskResponseDTO, HttpStatus httpStatus) {
+    }
+
+    public HttpStatusCode completeTask(TaskResponseDTO taskResponseDTO, HttpStatus httpStatus) {
+    }
+
+
+    public Object completeTask(String userid) {
+    }
+
+    public Object incompleteTask(String userid) {
+    }
 }
